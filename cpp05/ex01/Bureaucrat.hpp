4ff2a372be4be65e1465137d28f2f74cee697993
theoapp <116431821+TheoAppourchaux@users.noreply.github.com>
2024-05-26 18:13:36 +0200

    add the necessary files

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 20:19:20 by tappourc          #+#    #+#             */
+/*   Updated: 2024/05/25 20:19:21 by tappourc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat {
+
+public:
+
+	Bureaucrat(std::string name, int grade);
+	~Bureaucrat();
+	
+	Bureaucrat(Bureaucrat const &cpy);
+	Bureaucrat & operator=(Bureaucrat const &param);
+
+    std::string get_name() const;
+	int get_grade() const;
+	
+	void incrementGrade();
+    void decrementGrade();
+
+	class GradeTooHighException : public std::exception {
+    public:
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception {
+    public:
+        const char* what() const throw();
+    };
+
+private:
+        std::string const _name;
+		int _grade;
+
+		void valid_grade(int grade);
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
+
+#endif
