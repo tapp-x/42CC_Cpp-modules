47e9bb81ec6c8afc11494a02bde3225cda3b6d97
theoapp <116431821+TheoAppourchaux@users.noreply.github.com>
2024-05-26 18:31:47 +0200

    declare form class  (hpp part)

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -6,7 +6,47 @@
 /*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 20:41:03 by tappourc          #+#    #+#             */
-/*   Updated: 2024/05/25 20:41:04 by tappourc         ###   ########.fr       */
+/*   Updated: 2024/05/26 18:24:15 by tappourc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+public:
+    Form(const std::string& name, int signGrade, int execGrade);
+    ~Form();
+    
+    Form(const Form& copy);
+    Form& operator=(const Form& src);
+
+    std::string get_name() const;
+    bool is_signed() const;
+    int get_sign_grade() const;
+    int get_exec_grade() const;
+
+    void beSigned(const Bureaucrat& bureaucrat);
+
+    class GradeTooHighException : public std::exception {
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception {
+        const char* what() const throw();
+    };
+
+private:
+    const std::string   _name;
+    bool                _is_signed;
+    const int           _grade_to_sign;
+    const int           _grade_to_exec;
+};
+
+std::ostream& operator<<(std::ostream& out, const Form& form);
+
+#endif
