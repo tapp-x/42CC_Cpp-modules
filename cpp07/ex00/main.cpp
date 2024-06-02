563538249476a5f838f8c56aae70861d2b29cfc4
theoapp <116431821+TheoAppourchaux@users.noreply.github.com>
2024-06-02 12:47:22 +0200

    implement template and main tests

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 12:41:17 by tappourc          #+#    #+#             */
+/*   Updated: 2024/06/02 12:44:44 by tappourc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "whatever.hpp"
+
+int main() {
+    int a = 2;
+    int b = 3;
+
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+    return 0;
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 12:43:30 by tappourc          #+#    #+#             */
+/*   Updated: 2024/06/02 12:43:31 by tappourc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T const &min(T const &a, T const &b) {
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T const &max(T const &a, T const &b) {
+    return (a > b) ? a : b;
+}
+
+#endif
