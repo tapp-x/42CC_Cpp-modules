bf9d91c3c98b57145123d073a898346017990162
theoapp <116431821+TheoAppourchaux@users.noreply.github.com>
2024-10-15 17:30:38 +0200

    clean cpp08 & add print function on ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -9,17 +9,25 @@
 
 class NotFoundException : public std::exception {
 public:
-    const char* what() const throw() {
-        return ("Element not found");
-    }
+	const char* what() const throw() {
+		return ("Element not found");
+	}
 };
 
 template <typename T>
 typename T::iterator easyfind(T& cont, int value) {
-    typename T::iterator iter = std::find(cont.begin(), cont.end(), value);
-    if (iter == cont.end())
-        throw NotFoundException();
-    return (iter);
+	typename T::iterator iter = std::find(cont.begin(), cont.end(), value);
+	if (iter == cont.end())
+		throw NotFoundException();
+	return (iter);
+}
+
+template <typename T>
+void	print_all(T& cont) {
+	for (typename T::iterator it = cont.begin(); it != cont.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 }
 
 #endif
